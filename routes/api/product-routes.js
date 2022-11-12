15e3995a678153d49ec8de8390668348258e32cb
Vanese <vanesebuchanan@gmail.com>
2022-11-12 14:58:52 -0500

    updated route files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -10,12 +10,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [{
+      model: Category,
+      attributes: ["id", "category_name"],
+    },
+    {
+      model: Tag,
+      attribute: ["id", "tag_name"]
+    }
+  ]
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [{
+      model: Category,
+      attributes: ["id", "category_name"],
+    },
+    {
+      model: Tag,
+      attribute: ["id", "tag_name"]
+    }
+  ]
+
+  })
 });
 
 // create new product
